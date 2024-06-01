2ef693ef90f8f6a22ddac957676c4dab99dd6634
KarlJungtow <144035389+KarlJungtow@users.noreply.github.com>
2024-06-01 17:44:28 +0200

    Van kills enemies

--- mobs/van.gd
+++ mobs/van.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+@onready var enemies = $"../Enemies"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
@@ -13,8 +13,16 @@ func _process(delta):
 
 func _physics_process(delta):
 	position.x += delta * 200
-	
-
+	if enemies != null:
+		var children = enemies.get_children()
+		if children != null:
+			for child in children:
+				if position.x > child.position.x:
+					child.queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_body_entered(body):
+	print(body.name)
