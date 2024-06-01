e8a88ce087969e099c7efdc64203ef54135cfa78
mcromp <crompton.ml@gmail.com>
2024-06-01 16:40:20 +0200

    add animation to collectable

--- scripts/collectable.gd
+++ scripts/collectable.gd
@@ -1,6 +1,7 @@
-extends Area2D
+extends Node
 
-
+var is_alive = true
+@onready var anim = get_node("collectable_animation_player")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,6 +13,12 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and is_alive:
+		is_alive = false
+		anim.play('death')
 		G.handle_collectable.emit()
+
+
+func _on_collectable_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "death":
 		queue_free()
