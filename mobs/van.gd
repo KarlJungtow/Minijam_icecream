01f729170125937432ed2cb9ebb7732959caf692
mcromp <crompton.ml@gmail.com>
2024-06-01 18:59:38 +0200

    add enemy death

--- mobs/van.gd
+++ mobs/van.gd
@@ -22,5 +22,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func _on_area_entered(area):
 	if area.is_in_group("enemy"):
-		area.queue_free()
+		if area.die:
+			area.die()
+		else:
+			area.queue_free()
 
